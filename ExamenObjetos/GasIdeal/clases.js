d7cf77cbfa0f45683375c613f8c29ba2b7381cc9
Onixdrew <andrewmosquera1811@gmail.com>
2024-09-27 16:55:03 -0500

    commit de los gases

---
+++ ExamenObjetos/GasIdeal/clases.js
@@ -0,0 +1,31 @@
+class Gas {
+    constructor(presion, volumen, temperatura){
+        this.presion = presion,
+        this.volumen = volumen,
+        this.temperatura = temperatura
+    }
+    get presion(){
+        return this.presion
+    }
+    get volumen(){
+        return this.volumen
+    }
+    get temperatura(){
+        return this.temperatura
+    }
+
+    setpresion(presion){
+        this.presion=presion
+    }
+    setvolumen(volumen){
+        this.volumen = volumen
+    }
+    settemperatura(temperatura){
+        this.temperatura = temperatura
+    }
+    calcularMasa(masaMol){
+        let masaMol = masaMol
+        let masa = masa
+    }
+    
+}
